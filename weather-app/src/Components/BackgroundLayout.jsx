9b22a0d632122992ac4f00012a0492f83596a962
DrShade <dimadhehr@gmail.com>
2024-04-11 23:21:49 +0300

    oh

--- weather-app/src/Components/BackgroundLayout.jsx
+++ weather-app/src/Components/BackgroundLayout.jsx
@@ -1,10 +1,10 @@
-import Clear from "../assets/icons/clear.jpg"
-import Sunny from "../assets/icons/sunny.jpg"
-import Fog from 'https://www.bing.com/ck/a?!&&p=71fd4d525053e8c4JmltdHM9MTcxMjYyMDgwMCZpZ3VpZD0xOWVlMzYyYy1kYTM0LTZhMmQtM2Y5ZS0yN2E0ZGUzNDY4NmUmaW5zaWQ9NTU1MQ&ptn=3&ver=2&hsh=3&fclid=19ee362c-da34-6a2d-3f9e-27a4de34686e&u=a1L2ltYWdlcy9zZWFyY2g_cT1mb2clMjBpbWFnZSUyMGpwZyZGT1JNPUlRRlJCQSZpZD04MkE1Njg1NTc0RDZDRjkwMzY1ODVDNUREMDk5QzVDNTRDQzk2NEQ0&ntb=1'
-import Cloudy from "../assets/icons/cloudy.jpg";
-import Rainy from "../assets/icons/rain.jpg";
-import Snow from "../assets/icons/snowy.jpg";
-import Stormy from "../assets/icons/stormy.jpg";
+import Clear from "../assets/images/clear.jpg"
+import Sunny from "../assets/images/sunny.jpg"
+import Fog from "../assets/images/fog.jpg"
+import Cloudy from "../assets/images/cloudy.jpg";
+import Rainy from "../assets/images/rain.jpg";
+import Snow from "../assets/images/snowy.jpg";
+import Stormy from "../assets/images/stormy.jpg";
 import { useStateContext } from "../Context";
 import { useEffect, useState } from "react";
 
